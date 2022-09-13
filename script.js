d54d3703b54814d2e57c7028f8027917f8b32f1e
Guillermo San Martín <guille.seba0802@gmail.com>
2022-09-13 18:57:45 -0300

    arreglos

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ class Productos {
   }
 
   agregarAlCarrito() {
+    /*Pido el número del producto deseado*/
     let nuevoProducto = parseInt(
       prompt(
         "Elija el producto que desea agregar al carrito de compras:\n1 - Remera de Warrios USD$50.\n2 - Remera de Lakers USD$50.\n3 - Remera de Spurs USD$50.\nIngrese 0 para finalizar la compra."
@@ -30,15 +31,19 @@ class Productos {
     if (this.productosDeseados !== 0) {
       while (nuevoProducto !== 0) {
         if (this.productos[nuevoProducto]) {
+          /* Agrego el producto deseado al array de this.productosDeseados */
           this.productosDeseados.push(nuevoProducto);
           alert(`Se ha agregado el producto ${nuevoProducto} correctamente`);
+          /* Vuelvo a pedir producto deseado */
           nuevoProducto = parseInt(
             prompt(
               "Elija el producto que desea agregar al carrito de compras:\n1 - Remera de Warrios USD$50.\n2 - Remera de Lakers USD$50.\n3 - Remera de Spurs USD$50.\nIngrese 0 para finalizar la compra."
             )
           );
         } else {
+          /* Si el producto deseado no existe: */
           alert("Producto Inexistente.");
+          /* Vuelvo a pedir producto deseado */
           nuevoProducto = parseInt(
             prompt(
               "Elija el producto que desea agregar al carrito de compras:\n1 - Remera de Warrios USD$50.\n2 - Remera de Lakers USD$50.\n3 - Remera de Spurs USD$50.\nIngrese 0 para finalizar la compra."
@@ -53,11 +58,13 @@ class Productos {
   eliminarDelCarrito() {
     let eliminar = confirm("Desea eliminar un producto?");
     while (eliminar && this.productosDeseados.length > 0) {
+      /* Pido producto para eliminar */
       let productoAEliminar = parseInt(
         prompt(
           `Tu carrito: ${this.mostrarCarrito()}\nIngrese el numero de producto que desea eliminar`
         )
       );
+      /* Elimino el producto */
       this.productosDeseados.splice(
         this.productosDeseados.indexOf(productoAEliminar),
         1
@@ -69,6 +76,7 @@ class Productos {
   }
 
   mostrarCarrito() {
+    /* Uso los valores ingresados por el usuario para crear un carrito con nombre e id de los productos */
     let carrito = [];
     this.productosDeseados.forEach((prod) => {
       carrito += `\n${this.productos[prod].id} - ${this.productos[prod].nombre}`;
@@ -78,6 +86,7 @@ class Productos {
 
   comprarProducto() {
     if (this.productosDeseados.length > 0) {
+      /* Pido que ingrese el metodo de pago que desea */
       let metodoDePago = parseInt(
         prompt(
           "Elija el método de pago que desea utilizar:\n1 - Efectivo en red de cobranza.\n2 - Tarjeta de débito(Descuento del 20% en el total de su compra).\n3 - tarjeta de crédito(Hasta 12 cuotas sin recargo)"
@@ -86,6 +95,8 @@ class Productos {
 
       let precioTotal = 50 * this.productosDeseados.length;
       let cantidadDeCuotas;
+
+      /* Efectuo los descuentos e impuestos correspondientes, y la elección de cantidad de cuotas */
 
       if (metodoDePago === 1) {
         alert(`El precio total es de USD$${precioTotal}`);
