834b4dc8bcd518507c01cb9d93341702ee8c6c84
Guillermo San Martín <guille.seba0802@gmail.com>
2022-09-20 22:08:20 -0300

    mejorando el proyecto

--- script.js
+++ script.js
@@ -1,85 +1,79 @@
-/* Sección donde están los productos */
 const tienda = document.getElementById("tienda");
 const miCarrito = document.getElementById("carrito");
 
-class Productos {
-  constructor() {
-    this.productos = [
-      {
-        id: 1,
-        nombre: "Remera de Warriors",
-        precio: 50,
-      },
-      {
-        id: 2,
-        nombre: "Remera de Lakers",
-        precio: 50,
-      },
-      {
-        id: 3,
-        nombre: "Remera de Spurs",
-        precio: 50,
-      },
-    ];
-    this.productosDeseados = [];
-  }
+let stock = [
+  { id: 1, nombre: "Remera de Golden State Warriors", precio: 50 },
+  { id: 2, nombre: "Remera de San Antonio Spurs", precio: 35 },
+  { id: 3, nombre: "Remera de Los Angeles Lakers", precio: 45 },
+  { id: 4, nombre: "Remera de New Orleans Pelicans", precio: 35 },
+];
 
-  renderizarProductos() {
-    const producto = document.createElement("div");
-    this.productos.forEach((prod) => {
-      producto.innerHTML += `<article id="${prod.id}"><h3>${prod.nombre}</h3>
-      <p>Precio: ${prod.precio}</p><button class="carrito">Agregar al carrito</button></article>`;
-    });
-    tienda.append(producto);
-  }
+let carrito = JSON.parse(localStorage.getItem("item")) || [];
+let precioTotal = JSON.parse(localStorage.getItem("precio")) || null;
 
-  agregarAlCarrito(nuevoProd) {
-    this.productos.forEach((prod) => {
-      if (prod.id === nuevoProd) {
-        this.productosDeseados.push(prod);
-      }
-    });
-  }
+stock.forEach((prod) => {
+  const div = document.createElement("div");
+  div.classList.add("shop__prod");
+  div.innerHTML = `
+  <h3 class="prod__title">${prod.nombre}</h3>
+  <p>Precio: USD$${prod.precio}</p>
+  <button id="agregar${prod.id}" class="botonAgregar">Agregar Al Carrito</button>
+  `;
 
-  eliminarDelCarrito(prodAEliminar) {
-    this.productosDeseados.forEach((prod) => {
-      if (prod.id === prodAEliminar) {
-        let index = this.productosDeseados.findIndex(
-          (prod) => prodAEliminar === prod.id
-        );
-        this.productosDeseados.splice(index, 1);
-        console.log(this.productosDeseados);
-      }
-    });
-  }
+  tienda.appendChild(div);
 
-  mostrarCarrito() {
-    miCarrito.innerHTML = "";
-    this.productosDeseados.forEach((prod) => {
-      miCarrito.innerHTML += `<p id="${prod.id}">${prod.nombre}<button class="eliminar">Eliminar</button></p>\n`;
-    });
-  }
-}
+  const boton = document.getElementById(`agregar${prod.id}`);
 
-const prod1 = new Productos();
+  boton.addEventListener("click", (event) => {
+    agregarAlCarrito(prod.id);
+  });
+});
+
+const agregarAlCarrito = (prodId) => {
+  const item = stock.find((prod) => prod.id === prodId);
+  carrito.push(item);
+  precioTotal += item.precio;
+  guardarPrecio();
+  guardarCarrito();
+  mostrarCarrito();
+};
+
+const guardarPrecio = () =>
+  localStorage.setItem("precio", JSON.stringify(precioTotal));
+
+const guardarCarrito = () =>
+  localStorage.setItem("item", JSON.stringify(carrito));
+
+const mostrarCarrito = () => {
+  miCarrito.innerHTML = "";
+  miCarrito.innerHTML += "Carrito:";
+  carrito.forEach((prod) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <p>${prod.nombre}</p>
+    <p>Precio: ${prod.precio}</p>
+    <button class="botonEliminar" onclick="eliminarDelCarrito(${prod.id})">Eliminar del carrito</button>
+    `;
+    miCarrito.appendChild(div);
+  });
+  if (precioTotal > 0) {
+    miCarrito.innerHTML += `
+    <p class="precioTotal">Precio total: USD$${precioTotal}</p>
+    <button class="botonComprar">Comprar</button>
+    `;
+  }
+};
+
+const eliminarDelCarrito = (prodId) => {
+  const item = carrito.find((prod) => prod.id === prodId);
+  let indice = carrito.indexOf(item);
+  carrito.splice(indice, 1);
+  precioTotal -= item.precio;
+  guardarPrecio();
+  guardarCarrito();
+  mostrarCarrito();
+};
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  prod1.renderizarProductos();
-  const botonesAgregar = document.querySelectorAll(".carrito");
-
-  /* Mostrar carrito de compras al ir agregando productos */
-  botonesAgregar.forEach((boton) => {
-    boton.addEventListener("click", (event) => {
-      let prodId = boton.parentElement.id;
-      prod1.agregarAlCarrito(parseInt(prodId));
-      prod1.mostrarCarrito();
-      const botonesEliminar = document.querySelectorAll(".eliminar");
-      botonesEliminar.forEach((boton) => {
-        boton.addEventListener("click", (event) => {
-          let prodAEliminar = boton.parentElement.id;
-          prod1.eliminarDelCarrito(parseInt(prodAEliminar));
-        });
-      });
-    });
-  });
+  mostrarCarrito();
 });
